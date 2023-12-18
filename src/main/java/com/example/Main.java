8aad9d862683c4273bb69a291a13b64dce617020
lai2-24 <lai2-24@itismeucci.com>
2023-12-18 11:19:43 +0100

    secondo commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -12,64 +12,85 @@ public class Main {
             ServerSocket ss = new ServerSocket(8080);
 
             while (true) {
-                Socket s = ss.accept();
-                System.out.println("Un client si è connesso");
-
-                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                DataOutputStream out = new DataOutputStream(s.getOutputStream());
-
-                String linea1 = in.readLine();
-                System.out.println(linea1);
-                String vuota = "";
-                do {
-                    vuota = in.readLine();
-                    System.out.println(vuota);
-
-                } while (!vuota.equals(""));
-
-                // analizzare il contenuto di stringa2
-                String[] arrayLinea1 = linea1.split(" ");
-                System.out.println("La stringa numero 2 è composta da: " + arrayLinea1[1]);
-
-                
-
                 try {
+                    Socket s = ss.accept();
+                    System.out.println("Un client si è connesso");
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+                    String linea1 = in.readLine();
+                    System.out.println(linea1);
+                    String vuota = "";
+                    do {
+                        vuota = in.readLine();
+                        System.out.println(vuota);
+
+                    } while (!vuota.equals(""));
+
+                    // analizzare il contenuto di stringa2
+                    String[] arrayLinea1 = linea1.split(" ");
+                    System.out.println("La stringa numero 2 è composta da: " + arrayLinea1[1]);
+
                     File file = new File("htdocs" + arrayLinea1[1]);
-
-                    Scanner myReader = new Scanner(file);
-
-                    out.writeBytes("HTTP/1.1 200 OK" + "\n");
-                    out.writeBytes("Date: " + LocalDateTime.now().toString() + "\n");
-                    out.writeBytes("Content-Length: " + file.length() + "\n");
-                    out.writeBytes("Server: meucci-server" + "\n");
-                    out.writeBytes("Content-Type: text/html; charset=UTF-8" + "\n");
-                    out.writeBytes(" " + "\n");
-
-                    while (myReader.hasNextLine()) {
-                        out.writeBytes(myReader.nextLine()+ "\n");
+                    if (file.exists())
+                        sendBinaryFile(out, file);
+                    else {
+                        String msg = "The resource was not found";
+                        out.writeBytes("HTTP/1.1 404 Not Found" + "\n");
+                        out.writeBytes("Date: " + LocalDateTime.now().toString() + "\n");
+                        out.writeBytes("Server: meucci-server" + "\n");
+                        out.writeBytes("Content-Type: text/plain; charset=UTF-8" + "\n");
+                        out.writeBytes("Content-Length: " + msg.length() + "\n");
+                        out.writeBytes("" + "\n");
+                        out.writeBytes(msg + "\n");
                     }
-                    myReader.close();
-
-                } catch (FileNotFoundException e) {
-                    String msg = "The resource was not found";
-                    out.writeBytes("HTTP/1.1 404 Not Found" + "\n");
-                    out.writeBytes("Date: " + LocalDateTime.now().toString() + "\n");
-                    out.writeBytes("Server: meucci-server" + "\n");
-                    out.writeBytes("Content-Type: text/plain; charset=UTF-8" + "\n");
-                    out.writeBytes("Content-Length: " + msg.length() + "\n");
-                    out.writeBytes(" " + "\n");
-                    out.writeBytes(msg + "\n");
-
+                    s.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                s.close();
-
             }
-
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
             System.out.println("Errore");
             System.exit(1);
         }
 
+    }
+
+    public static void sendBinaryFile(DataOutput output, File file) throws IOException {
+        output.writeBytes("HTTP/1.1 200 OK" + "\n");
+        output.writeBytes("Content-Length: " + file.length() + "\n");
+        output.writeBytes("Content-Type: " + getContentType(file) + "\n");
+        output.writeBytes("\n");
+        InputStream input = new FileInputStream(file);
+        byte[] buf = new byte[8192];
+        int n;
+        while ((n = input.read(buf)) != -1) {
+            output.write(buf, 0, n);
+        }
+        input.close();
+    }
+
+    public static String getContentType(File f) {
+        String[] s = f.getName().split("\\.");
+        String ext = s[s.length - 1];
+        switch (ext) {
+            case "html":
+                return "text/html";
+            case "css":
+                return "text/css";
+            case "png":
+                return "image/png";
+            case "jpg":
+                return "image/jpg";
+            case "jpeg":
+                return "image/jpeg";
+            case "gif":
+                return "image/gif";
+            case "js":
+                return "test/js";
+        }
+        return "text/plain";
     }
 }
